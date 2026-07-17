240e5e4aaea5f22482a1b91e7b4b4f0c23c9dbf9
Anish9345 <cocanish1234@gmail.com>
2026-07-17 11:41:44 +0530

    Added user Schema _01

--- Backend/13July/controller/user.controller.js
+++ Backend/13July/controller/user.controller.js
@@ -1,6 +1,17 @@
-const userHome = (req, res) => {
-    res.json("User Home Page");
+// const userHome = (req, res) => {
+//     res.json("User Home Page");
+// }
+
+const User = require("../model/user.schema");
+
+const userHome = async (req, res) => {
+    const userData = User.find().limit(10);
+    res.json({
+        status: "Success",
+        data: userData
+    });
 }
+
 
 module.exports = {
     userHome
